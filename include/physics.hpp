b1018aba25af9469ced48ca1ba4a842307316747
quartascode <brenoposavac@proton.me>
2026-07-05 05:10:17 -0300

    add physics attributes

--- include/physics.hpp
+++ include/physics.hpp
@@ -1,4 +1,12 @@
+#include "world.hpp"
+
 class Physics {
 	public:
+		Physics(WorldBounds& bounds)
+			: gravityAccel({0, -9.81}),
+			worldBounds(bounds)
+		{}
 	private:
+		Vec2 gravityAccel;
+		WorldBounds worldBounds;
 };
